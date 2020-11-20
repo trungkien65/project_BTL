5f5dc8fc5f5516c9249c25db7e24ffe01af138b5
trungkien65 <buithekien651999@gmail.com>
2020-11-20 22:24:42 +0700

    update

--- MVC/backend/index.php
+++ MVC/backend/index.php
@@ -12,20 +12,17 @@ $controller .= "Controller";
 
 $path_controller = "controllers/$controller.php";
 
-
-//kiểm tra nếu đường dẫn ko tồn tại, thì báo trang ko tồn tại
 if (file_exists($path_controller) == false) {
   die('Trang bạn tìm không tồn tại');
 }
 
 require_once "$path_controller";
 
-//khởi tạo đối tượng sau khi nhúng file
-$object = new $controller(); //$object = new BookController()
+$object = new $controller();
 
 if (method_exists($object, $action) == false) {
   die("Không tồn tại phương thức $action của class $controller");
 }
-//index.php?controller=book&action=create
+
 $object->$action();
 ?>
